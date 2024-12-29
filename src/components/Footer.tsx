f67198fe4d016a9cb0491a6072237d6ad491c3b2
krish <krishnavishwakarma0808@gmail.com>
2024-12-29 23:19:54 +0530

    `Update Footer component with new content and formatting`

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <Box as="footer" bg={"blackAlpha.800"} id='footer'>
+    <Box as="footer" bg={"blackAlpha.800"} id="footer">
       <Box px={12} py={8} mx={"auto"} maxW={"8xl"}>
         <SimpleGrid columns={[1, 3]} gap={12}>
           <Stack gap={4}>
@@ -11,10 +11,13 @@ const Footer = () => {
               SRI BHARATH FOUNDRY
             </Text>
             <Text fontSize="small" color="white">
-              Established since 1989, Welding and Iron works has become one of
-              the most respected structural, miscellaneous, and ornamental steel
-              fabricators serving the Southern California multi-unit,
-              commercial, and residential{" "}
+              At Sri Bharath Foundry, we are committed to delivering
+              precision-engineered grey cast iron products that exceed industry
+              standards. Our dedication to craftsmanship and customer
+              satisfaction ensures that every component we produce is reliable,
+              durable, and tailored to meet the specific needs of our clients.
+              Partner with us and experience the quality and innovation that
+              make us a trusted name in the industry.
             </Text>
           </Stack>
           <Box></Box>
@@ -26,19 +29,40 @@ const Footer = () => {
               Us
             </Text>
             <Text fontSize="md" color="white">
-            Phone: <Link href='tel:9360098007' color={"yellow.300"}>+91 93600 98007</Link> or <Link href='tel:6369505151' color={"yellow.300"}>+91 63695 05151</Link>
+              Phone:{" "}
+              <Link href="tel:9360098007" color={"yellow.300"}>
+                +91 93600 98007
+              </Link>{" "}
+              or{" "}
+              <Link href="tel:6369505151" color={"yellow.300"}>
+                +91 63695 05151
+              </Link>
             </Text>
             <Text fontSize="md" color="white">
-            Email: <Link href="mailto:sribharathfoundry@gmail.com" color={"yellow.300"}>sribharathfoundry@gmail.com</Link>
+              Email:{" "}
+              <Link
+                href="mailto:sribharathfoundry@gmail.com"
+                color={"yellow.300"}
+              >
+                sribharathfoundry@gmail.com
+              </Link>
             </Text>
             <Text fontSize="md" color="white">
-            Address: <Link display={"inline"} href="https://maps.app.goo.gl/sLCPvSoTT5opEvA8A" color={"yellow.300"} >S.F. No. 257/1C, Pachapalayam Village, Annur Taluk, Arasur Foundry Cluster, Coimbatore - 641 653</Link>
+              Address:{" "}
+              <Link
+                display={"inline"}
+                href="https://maps.app.goo.gl/sLCPvSoTT5opEvA8A"
+                color={"yellow.300"}
+              >
+                S.F. No. 257/1C, Pachapalayam Village, Annur Taluk, Arasur
+                Foundry Cluster, Coimbatore - 641 653
+              </Link>
             </Text>
           </Stack>
         </SimpleGrid>
       </Box>
     </Box>
-  )
+  );
 }
 
 export default Footer
