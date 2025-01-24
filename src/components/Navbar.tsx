56b3558a923f95e439a4eaa572b5211303c779fc
Sandeep Vishvakarma <sandee.vit14@gmail.com>
2025-01-24 17:25:00 +0530

    added menu button in mobile view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,29 @@
-import { Box, Button, Image, Link, Heading, Span, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Image,
+  Link,
+  Heading,
+  Span,
+  useBreakpointValue,
+  IconButton,
+  Stack,
+} from "@chakra-ui/react";
 import Logo from "../assets/images/logo.png";
+import { useCallback, useState } from "react";
+import { motion } from "framer-motion";
+
+const MotionBox = motion(Box);
+
 export const Navbar = () => {
-  const isMobile = useBreakpointValue({base: true, md:false}) 
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
+  const [show, setShow] = useState(false);
+
+  const handleMenu = useCallback(() => {
+    setShow(!show);
+  }, [show]);
+
   return (
     <Box
       as={"header"}
@@ -10,51 +32,186 @@ export const Navbar = () => {
       display="flex"
       justifyContent="space-between"
       alignItems="center"
-      marginBottom={isMobile ? 1:2}
+      marginBottom={isMobile ? 1 : 2}
+      pos={show ? "fixed" : "relative"}
+      zIndex={4}
     >
       <Box>
-        <Image src={Logo} width={isMobile ?"20":"24"} height={isMobile ?"20":"24"} alt="Sri Bharath Foundry Logo" />
+        <Image
+          src={Logo}
+          width={isMobile ? "20" : "24"}
+          height={isMobile ? "20" : "24"}
+          alt="Sri Bharath Foundry Logo"
+        />
       </Box>
 
       <Box>
-        <Heading fontSize= {isMobile ? "xl":"4xl"} paddingRight={8}>           
+        <Heading fontSize={isMobile ? "xl" : "4xl"} paddingRight={8}>
           <Span color={"blue.800"}>SRI BHARATH FOUNDRY</Span>
         </Heading>
       </Box>
-{isMobile ||
-      <Box
-        as="ul"
-        display="flex"
-        justifyContent="center"
-        alignItems={"center"}
-        listStyleType="none"
-        gap={4}
-      >
-        <Box as="li" paddingX={2}>
-          <Link as="a" href="#about" textDecoration="none" color="gray.600">
-            About
-          </Link>
+      {isMobile && (
+        <>
+          <IconButton
+            onClick={handleMenu}
+            width="30px"
+            height="24px"
+            backgroundColor={"transparent"}
+            p={0}
+            zIndex={3}
+          >
+            <Stack position="relative" width="30px" height="24px">
+              <MotionBox
+                width="100%"
+                height="2px"
+                bg={show ? "white" : "black"}
+                borderRadius="md"
+                initial={{ y: 0, rotate: 0 }}
+                animate={
+                  show ? { y: 9, x: 1, rotate: 45 } : { y: 0, rotate: 0 }
+                }
+                transition={{ duration: 0.3 }}
+              />
+
+              <MotionBox
+                width="100%"
+                height="2px"
+                bg={show ? "white" : "black"}
+                borderRadius="md"
+                initial={{ opacity: 1 }}
+                animate={show ? { opacity: 0 } : { opacity: 1 }}
+                transition={{ duration: 0.3 }}
+              />
+              <MotionBox
+                width="100%"
+                height="2px"
+                bg={show ? "white" : "black"}
+                borderRadius="md"
+                initial={{ y: 0, rotate: 0 }}
+                animate={
+                  show ? { y: -12, x: 1, rotate: -45 } : { y: 0, rotate: 0 }
+                }
+                transition={{ duration: 0.3 }}
+              />
+            </Stack>
+          </IconButton>
+          <Box
+            style={{ display: show ? "flex" : "none" }}
+            bg={"blackAlpha.900"}
+            pos={"fixed"}
+            top={0}
+            left={0}
+            height={"100vh"}
+            width={"100vw"}
+            zIndex={2}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            overflow={"hidden"}
+          >
+            <Box
+              as="ul"
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems={"center"}
+              listStyleType="none"
+              gap={8}
+              flex={1}
+              height={"100%"}
+            >
+              <Box as="li" paddingX={2}>
+                <Link
+                  as="a"
+                  href="#about"
+                  textDecoration="none"
+                  color="gray.100"
+                  onClick={handleMenu}
+                >
+                  About
+                </Link>
+              </Box>
+              <Box as="li" paddingX={2}>
+                <Link
+                  as="a"
+                  href="#products"
+                  textDecoration="none"
+                  color="gray.100"
+                  onClick={handleMenu}
+                >
+                  Products
+                </Link>
+              </Box>
+              <Box as="li" paddingX={2}>
+                <Link
+                  as="a"
+                  href="#clients"
+                  textDecoration="none"
+                  color="gray.100"
+                  onClick={handleMenu}
+                >
+                  Clients
+                </Link>
+              </Box>
+              <Box as="li" paddingX={2}>
+                <Link
+                  as="a"
+                  href="#footer"
+                  textDecoration="none"
+                  color="yellow.300"
+                >
+                  <Button
+                    bg={"yellow.300"}
+                    color={"blackAlpha.700"}
+                    variant="solid"
+                    onClick={handleMenu}
+                  >
+                    Contact
+                  </Button>
+                </Link>
+              </Box>
+            </Box>
+          </Box>
+        </>
+      )}
+      {!isMobile && (
+        <Box
+          as="ul"
+          display="flex"
+          justifyContent="center"
+          alignItems={"center"}
+          listStyleType="none"
+          gap={4}
+        >
+          <Box as="li" paddingX={2}>
+            <Link as="a" href="#about" textDecoration="none" color="gray.600">
+              About
+            </Link>
+          </Box>
+          <Box as="li" paddingX={2}>
+            <Link
+              as="a"
+              href="#products"
+              textDecoration="none"
+              color="gray.600"
+            >
+              Products
+            </Link>
+          </Box>
+          <Box as="li" paddingX={2}>
+            <Link as="a" href="#clients" textDecoration="none" color="gray.600">
+              Clients
+            </Link>
+          </Box>
+          <Box as="li" paddingX={2}>
+            <Link as="a" href="#footer" textDecoration="none" color="gray.600">
+              <Button colorScheme="teal" variant="solid">
+                Contact
+              </Button>
+            </Link>
+          </Box>
         </Box>
-        <Box as="li" paddingX={2}>
-          <Link as="a" href="#products" textDecoration="none" color="gray.600">
-            Products
-          </Link>
-        </Box>
-        <Box as="li" paddingX={2}>
-          <Link as="a" href="#clients" textDecoration="none" color="gray.600">
-            Clients
-          </Link>
-        </Box>
-        <Box as="li" paddingX={2}>
-        <Link as="a" href="#footer" textDecoration="none" color="gray.600">
-          <Button colorScheme="teal" variant="solid" >
-            Contact
-          </Button>
-        </Link>
-        </Box>
-      </Box>}
+      )}
     </Box>
   );
 };
-
-
